7bff1f229240987008b6f2ecf80bb175260749e1
sjisu7525 <sjisu7525@gmail.com>
2025-04-06 17:54:44 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,32 +1,28 @@
-def cal(a,b,op) :
-    if op == "+" :
-        return a+b
-    elif op == "-" :
-        return a-b
-    elif op == "*" :
-        return a*b
-    elif op == "%":
-        if a >= 0 :
-            return a//b
-        else :
-            return -((-a)//b)
-
 n = int(input())
 num = list(map(int, input().split()))
-op_num = list(map(int, input().split())) # 덧셈(+)의 개수, 뺄셈(-)의 개수, 곱셈(×)의 개수, 나눗셈(÷)의 개수
-op = ["+","-","*","%"]
-op_list = []
-for o, op_n in zip(op, op_num) :
-    op_list += [o]*op_n
+ops = list(map(int, input().split()))
+min_val = float('inf')
+max_val = -float('inf')
 
-from itertools import permutations
-min_result = float('inf')
-max_result = -float('inf')
-for p in set(permutations(op_list, n-1)):
-    result = cal(num[0], num[1], p[0])
-    for i in range(1,n-1) :
-        result = cal(result, num[i+1], p[i])
-    min_result = min(min_result, result)
-    max_result = max(max_result, result)
-print(max_result)
-print(min_result)
+def dfs(depth, total, plus, minus, mul, div) :
+    global min_val, max_val
+    if depth == n :
+        min_val = min(min_val, total)
+        max_val = max(max_val, total)
+        return
+
+    if plus :
+        dfs(depth+1, total+num[depth], plus-1, minus, mul, div)
+    if minus :
+        dfs(depth+1, total-num[depth], plus, minus-1, mul, div)
+    if mul :
+        dfs(depth+1, total*num[depth], plus, minus, mul-1, div)
+    if div :
+        if total >= 0 :
+            dfs(depth+1, total//num[depth], plus, minus, mul, div-1)
+        else :
+            dfs(depth + 1, -((-total)//num[depth]), plus, minus, mul, div - 1)
+
+dfs(1, num[0], *ops)
+print(max_val)
+print(min_val)
